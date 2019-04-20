c017caf3c0cfb33fef59295d8817fa3b8db2921e
Super Nova <muhamadduki@gmail.com>
2019-04-21 00:59:27 +0800

    05 Styling and User Experience Score Keeper App - add server/main.js for DDP client

--- server/main.js
+++ server/main.js
@@ -1,5 +1,38 @@
 import { Meteor } from 'meteor/meteor'
-import { Players } from './../imports/api/players'
+// import { Players } from './../imports/api/players'
+
+// khusus untuk client DDP
+// ----------------------------------------------
+Meteor.methods({
+  clientProcessData: function (data) {
+    console.log(data);
+  
+    Players.insert(
+      {
+        name: data,
+        score: 3,
+      }
+    )    
+    // Meteor post data to MongoDB code here
+  }
+})
+
+if(Meteor.isServer) {
+  Meteor.publish('players', function() {
+     return Players.find();
+  })
+}
+
+if (Meteor.isClient) {
+    Meteor.subscribe('players');
+};
+
+Meteor.setTimeout(function() {
+    var myLog = Players.find().fetch();
+    console.log(myLog);
+}, 1000);
+// ----------------------------------------------
+
 
 Meteor.startup(() => {
   // let obj = {
